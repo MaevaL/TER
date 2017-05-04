db0ba6c97b0a08cf2a881da7cb126233156d843a
toopick <alexandre-1313@hotmail.fr>
2017-05-04 17:25:13 +0200

    ajout des pages pour gérer les utilisateurs, ajout de chosen pour les select avec barre de recherche

---
+++ src/UserBundle/Form/UserType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class, array(
+                'label' => 'Prénom',
+            ))
+            ->add('lastname', TextType::class, array(
+                'label' => 'Nom',
+            ))
+            ->add('numEtu', TextType::class, array(
+                'label' => 'Numéro étudiant',
+                'required' => false,
+            ))
+            ->add('email', TextType::class, array(
+                'label' => 'Email',
+            ))
+            ->add('roles', ChoiceType::class, array(
+                'label' => "Rôle de l'utilisateur",
+                'choices'   => array(
+                    'Etudiant'   => 'ROLE_USER',
+                    'Professeur'        => 'ROLE_ADMIN',
+                    'Super Utilisateur'        => 'ROLE_SUPER_ADMIN',
+                ),
+                'multiple' => true,
+            ))
+            ->add('ues', EntityType::class, array(
+                'label' => "UEs associées",
+                'class' => 'AppBundle:UE',
+                'choice_label' => 'name',
+                'attr' => array(
+                    'class' => 'chosen-select-ue'
+                ),
+                'multiple' => true,
+            ))
+        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'userbundle_user';
+    }
+
+
+}
